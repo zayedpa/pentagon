f0612a379495d379f553f9cea92cc2d3d7acace4
Zayedpa <142224095+zayedpa@users.noreply.github.com>
2025-07-03 09:55:35 +0530

    Add files via upload

---
+++ c01.py
@@ -0,0 +1,12 @@
+str1="rumaan"
+str2="midlaj"
+str3="appus"
+str4="asgu"
+str5="rumaan"
+str6="Midlaj"
+print(id(str1))
+print(id(str5))
+print(id(str3))
+print(id(str6))
+print(id(str2))
+print(id(str4))
