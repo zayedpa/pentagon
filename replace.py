9322db1f1cb1f54501608e2ab7590e7cb0d44c34
Zayedpa <142224095+zayedpa@users.noreply.github.com>
2025-07-03 09:56:10 +0530

    Add files via upload

---
+++ replace.py
@@ -0,0 +1,12 @@
+str="Rumaan Weds Faza"
+print(str)
+
+str1=str.replace("Weds","Bye")
+print(str1)
+
+print(str.startswith("Rumaan"))
+print(str.startswith("Asgar"))
+
+print(str.endswith("Najla"))
+print(str.endswith("Faza"))
+
