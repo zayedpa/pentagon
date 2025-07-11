15120919225d0867167c950352ced522a6c8a7e1
Zayedpa <142224095+zayedpa@users.noreply.github.com>
2025-07-11 21:31:51 +0530

    Add files via upload

---
+++ count.py
@@ -0,0 +1,13 @@
+str="if you think you can or you can't you are right"
+print(str)
+print(str.count("you"))
+      
+str1="you"
+print(str1 in str)
+print(str.index("you"))
+print(str.find("you"))
+
+print(str.rfind("you"))
+print(str.rindex("you"))
+print(str.find("yu"))
+print(str.index("yu"))
