c6d18e0935a7a465d9f41a3f11a1a15fa5d45409
Zayedpa <142224095+zayedpa@users.noreply.github.com>
2025-07-13 21:14:44 +0530

    Add files via upload

---
+++ ascii.py
@@ -0,0 +1,12 @@
+
+# character ascii checking
+char=input("enter a alpha")
+res=ord(char)  #ord to check ascii char num
+print(res)
+
+# number ascii checking
+
+num=int(input("enter a number"))
+res1=chr(num)
+print(res1)
+
